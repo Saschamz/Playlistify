a006bd816c5d5055f3ceb4fa1e64876bba9ee97e
Sascha Ringström <sascharingstrom@gmail.com>
2018-06-25 07:04:06 +0200

    Added some html/css for the player

--- src/SplashScreen/Statistics.jsx
+++ src/SplashScreen/Statistics.jsx
@@ -5,12 +5,14 @@ export default class Statistics extends Component {
     componentWillMount() {
         const uniqueArtists = [];
         this.songs = this.props.playlist.items.length;
-        this.artist = 0, this.duration = 0;
+        this.artist = 0, this.duration = 0, this.popularityPerSong;
         this.props.playlist.tracks.items.forEach(item => {
+            this.popularityPerSong += item.popularity;
             this.duration += item.track.duration_ms;
             let artistName = item.track.artists[0].name;
             !uniqueArtists.includes(artistName) && ++this.artist && uniqueArtists.push(artistName);
         });
+        this.popularityPerSong /= this.songs;
         const m_duration = this.duration / 1000 / 60;
         const h_duration = this.duration / 1000 / 60 / 60;
         if(h_duration >= 1) {
@@ -23,10 +25,22 @@ export default class Statistics extends Component {
     render() {
         return (
             <div className="statistics">
-                <div className="statistics__stat">{this.artists}</div>
-                <div className="statistics__stat">{this.songs}</div>
-                <div className="statistics__stat">{/*this.undecided*/}</div>
-                <div className="statistics__stat">{this.duration}</div>
+                <div className="statistics__stat">
+                    <h3 className="statistics__stat__header">{this.artists}</h3>
+                    <span className="statistics__stat__tooltip">artists</span>
+                </div>
+                <div className="statistics__stat">
+                    <h3 className="statistics__stat__header">{this.songs}</h3>
+                    <span className="statistics__stat__tooltip">songs</span>
+                </div>
+                <div className="statistics__stat">
+                    <h3 className="statistics__stat__header">{this.popularityPerSong}</h3>
+                    <span className="statistics__stat__tooltip">average popularity</span>
+                </div>
+                <div className="statistics__stat">
+                    <h3 className="statistics__stat__header">{this.duration}</h3>
+                    <span className="statistics__stat__tooltip">duration</span>
+                </div>
             </div>
         );
     }
